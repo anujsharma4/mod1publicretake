623ea1156e08ac80a8e4f2738e54c314afe6c1d9
github username <github email address>
2018-12-06 13:01:14 -0500

    AnujSharma

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
 class Article
 
+  attr_accessor :name, :author, :magazine
+  @@all = []
+
+  def initialize(name, magazine, author)
+    @name = name
+    @author = author
+    @magazine = magazine
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,9 +2,44 @@ class Author
 
   attr_accessor :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def articles
+    # Article.author
+    #returns an array of the article instance
+    #that the author has written
+    #iterate over the article arrays
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    #find articles that the author has written
+    self.articles.collect do |article|
+      article.magazine
+    end
+    #list out the magazines
+  end
+
+  def add_article(title, magazine)
+    new_article = Article.new(self, title, magazine)
+    return new_article
+  end
+
+  def find_specialties
+    self.articles.collect do |article|
+      article.magazine.category
+    end
   end
 
 end
